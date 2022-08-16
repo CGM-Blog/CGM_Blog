11ea045ec7ca63142cb5c24170b5d898e3f4b799
sangeun815 <grace0857@naver.com>
2022-08-16 12:46:22 +0900

    test

---
+++ posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    author = models.CharField(max_length=20)
+    file_download = models.ImageField(upload_tp='item_pics', blank = True)
+    dt_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
+    dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now=True)
+
+    def __str__(self):
+        return self.title
