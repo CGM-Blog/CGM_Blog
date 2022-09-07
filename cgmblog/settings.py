500143c10a1f0a30d7b73fce587259251e157318
minjko <komj36@gmail.com>
2022-09-07 13:31:44 +0900

    allauth 초기

--- cgmblog/settings.py
+++ cgmblog/settings.py
@@ -39,12 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
 
     'crispy_forms',
     'markdownx',
 
-
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount', 
